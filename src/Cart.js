3dd80e56fee45533f78b5942ceff47d3a160920f
SugarCubE3643 <vinayak.sahu01@gmail.com>
2023-02-07 12:04:13 +0530

    Adding decrease quantity and delete functionality

--- src/Cart.js
+++ src/Cart.js
@@ -44,12 +44,46 @@ class Cart extends React.Component {
       ],
     };
   }
+  handleIncreaseQuantity = (product) => {
+    const { products } = this.state;
+    const index = products.indexOf(product);
+
+    products[index].qty += 1;
+
+    this.setState({
+      products
+    });
+  }
+  handleDecreaseQuantity = (product) => {
+    const { products } = this.state;
+    const index = products.indexOf(product);
+
+    if(products[index].qty > 0){
+      products[index].qty -=1;
+      this.setState({products});
+    }
+  }
+  handleDeleteProduct = (id) => {
+    const { products } = this.state;
+
+    const items = products.filter( (item) => item.id !== id );
+
+    this.setState({
+      products: items
+    });
+  }
   render() {
     const { products } = this.state;
     return (
       <div className="cart">
         {products.map((product) => {
-          return <CartItem product={product} key={product.id} />;
+          return <CartItem 
+            product={product} 
+            key={product.id} 
+            onIncreaseQuantity={this.handleIncreaseQuantity} 
+            onDecreaseQuantity={this.handleDecreaseQuantity}
+            onDeleteProduct={this.handleDeleteProduct}
+          />;
         })}
       </div>
     );
--- src/CartItem.js
+++ src/CartItem.js
@@ -4,38 +4,40 @@ import deleteIcon from "./delete.png";
 import React from "react";
 
 class CartItem extends React.Component {
-  increaseQuantity = () => {
-    // Set state uses shallow merging and then the component is re rendered (setState is asynchronus)
-    // Set state acts as synchronously inside api calls and promises etc
+  // increaseQuantity = () => {
+  // Set state uses shallow merging and then the component is re rendered (setState is asynchronus)
+  // Set state acts as synchronously inside api calls and promises etc
 
-    // setState form 1 (When the state is not dependent on previous state)
+  // setState form 1 (When the state is not dependent on previous state)
 
-    // In case of multiple setStates react uses batching to render the component only once
-    // The calls are merged shallowly so react will take only the last returned object for updating the state
+  // In case of multiple setStates react uses batching to render the component only once
+  // The calls are merged shallowly so react will take only the last returned object for updating the state
 
-    // this.setState({
-    //   qty: this.state.qty + 1,
-    // }, () => {}); There is an option for a callback after state is finished updated
+  // this.setState({
+  //   qty: this.state.qty + 1,
+  // }, () => {}); There is an option for a callback after state is finished updated
 
-    // setState form 2 (When the state is dependent on previous state)
+  // setState form 2 (When the state is dependent on previous state)
 
-    // In case of multiple setStates react uses batching to render the component only once
-    // The calls are merged but react makes sure the prevState is up to date so the amount will update after every setState
+  // In case of multiple setStates react uses batching to render the component only once
+  // The calls are merged but react makes sure the prevState is up to date so the amount will update after every setState
 
-    this.setState((prevState) => {
-      return {
-        qty: prevState.qty + 1,
-      };
-    });
-  };
+  //   this.setState((prevState) => {
+  //     return {
+  //       qty: prevState.qty + 1,
+  //     };
+  //   });
+  // };
 
-  decreaseQuantity = () => {
-    this.setState((prevState) => {
-      return prevState.qty > 0 ? { qty: prevState.qty - 1 } : {};
-    });
-  };
+  // decreaseQuantity = () => {
+  //   this.setState((prevState) => {
+  //     return prevState.qty > 0 ? { qty: prevState.qty - 1 } : {};
+  //   });
+  // };
   render() {
     const { price, title, qty } = this.props.product;
+    const { product, onIncreaseQuantity, onDecreaseQuantity, onDeleteProduct } =
+      this.props;
     return (
       <div className="cart-item">
         <div className="left-block">
@@ -51,15 +53,20 @@ class CartItem extends React.Component {
               alt="increase"
               className="action-icons"
               src={increaseIcon}
-              onClick={this.increaseQuantity}
+              onClick={() => onIncreaseQuantity(product)}
             />
             <img
               alt="decrease"
               className="action-icons"
               src={decreaseIcon}
-              onClick={this.decreaseQuantity}
+              onClick={() => onDecreaseQuantity(product)}
             />
-            <img alt="delete" className="action-icons" src={deleteIcon} />
+            <img
+              alt="delete"
+              className="action-icons"
+              src={deleteIcon}
+              onClick={() => onDeleteProduct(product.id)}
+            />
           </div>
         </div>
       </div>
